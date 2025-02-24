c71f9ab019c9f87cff3531b1c443486a708ba5ab
Anurag Bansal <devanuragbansal@gmail.com>
2025-02-25 02:13:01 +0530

    Signup and JWT done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,45 @@
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+
 export const signup = async (req, res) => {
-  res.send("signup route");
+  const { email, fullName, password } = req.body;
+  if (!email || !fullName || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  if (password.length < 6) {
+    return res
+      .status(400)
+      .json({ message: "Password must be at least 6 characters long" });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      email,
+      fullName,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+      res.status(201).json({ message: "User created successfully" });
+    } else {
+      res.status(400).json({ message: "Invalid user data" }); 
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
 export const login = async (req, res) => {
--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,8 @@ import connectDB from "./lib/db.js";
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
